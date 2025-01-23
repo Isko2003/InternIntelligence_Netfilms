e40f69c2b7a16ae88b62d50432a032f4a4934ef1
Isko2003 <theazeisko2003@gmail.com>
2025-01-23 14:23:45 +0400

    initial commit

---
+++ .next/server/vendor-chunks/@next.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@next";
+exports.ids = ["vendor-chunks/@next"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/@next/font/google/target.css?{\"path\":\"app\\\\layout.jsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"interFontFamily\"}":
+/*!********************************************************************************************************************************************************************!*\
+  !*** ./node_modules/@next/font/google/target.css?{"path":"app\\layout.jsx","import":"Inter","arguments":[{"subsets":["latin"]}],"variableName":"interFontFamily"} ***!
+  \********************************************************************************************************************************************************************/
+/***/ ((module) => {
+
+eval("// Exports\nmodule.exports = {\n\t\"style\": {\"fontFamily\":\"'__Inter_d65c78', '__Inter_Fallback_d65c78'\",\"fontStyle\":\"normal\"},\n\t\"className\": \"__className_d65c78\"\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvQG5leHQvZm9udC9nb29nbGUvdGFyZ2V0LmNzcz97XCJwYXRoXCI6XCJhcHBcXFxcbGF5b3V0LmpzeFwiLFwiaW1wb3J0XCI6XCJJbnRlclwiLFwiYXJndW1lbnRzXCI6W3tcInN1YnNldHNcIjpbXCJsYXRpblwiXX1dLFwidmFyaWFibGVOYW1lXCI6XCJpbnRlckZvbnRGYW1pbHlcIn0iLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBLFdBQVcsZ0ZBQWdGO0FBQzNGO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9uZXRmaWxtcy8uL25vZGVfbW9kdWxlcy9AbmV4dC9mb250L2dvb2dsZS90YXJnZXQuY3NzPzExZTciXSwic291cmNlc0NvbnRlbnQiOlsiLy8gRXhwb3J0c1xubW9kdWxlLmV4cG9ydHMgPSB7XG5cdFwic3R5bGVcIjoge1wiZm9udEZhbWlseVwiOlwiJ19fSW50ZXJfZDY1Yzc4JywgJ19fSW50ZXJfRmFsbGJhY2tfZDY1Yzc4J1wiLFwiZm9udFN0eWxlXCI6XCJub3JtYWxcIn0sXG5cdFwiY2xhc3NOYW1lXCI6IFwiX19jbGFzc05hbWVfZDY1Yzc4XCJcbn07XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/@next/font/google/target.css?{\"path\":\"app\\\\layout.jsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"interFontFamily\"}\n");
+
+/***/ })
+
+};
+;
---
+++ app/movie/[id]/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MovieContainer from "@/containers/home/movie";
+import { notFound } from "next/navigation";
+import { getMovie } from "@/services/movie";
+
+async function MoviePage({ params, searchParams }) {
+  const movieDetail = await getMovie(params.id);
+  if (!movieDetail) {
+    notFound();
+  }
+
+  return <MovieContainer movie={movieDetail} />;
+}
+
+export default MoviePage;
